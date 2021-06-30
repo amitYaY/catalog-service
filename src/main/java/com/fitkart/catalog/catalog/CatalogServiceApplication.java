d2aa85f19e37b768537254d21e0c17f13a5d407f
Amit Upadhyay <upadhyay_amit@yahoo.com>
2021-06-30 05:47:33 +0530

    Repo Setup

---
+++ src/main/java/com/fitkart/catalog/catalog/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fitkart.catalog.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
